0cca981f0d10fe7cf2fb9d436d4577bbfa1a13c1
Curt <curtis.larssen@gmail.com>
2020-07-23 15:40:23 +1200

    Home - Responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Hero = styled.div`
   display:flex;
   justify-content:center;
   align-items:center;
-  background-color:${props=>props.theme.background};
+  background-color:${props => props.theme.background};
 `;
 
 const TextWrapper = styled.div`
@@ -19,15 +19,27 @@ text-align:center;
 
 const Header = styled.h1`
 font-family:'raleway';
-font-size:4rem;
+font-size:3.5rem;
 font-weight:400;
+@media (max-width: 675px) {
+font-size:2rem;
+  }
+  @media (max-width: 500px) {
+font-size:1.5rem;
+  }
 `
 
 const SubHeader = styled.h1`
 font-family:'raleway';
-font-size:2.5rem;
+font-size:2.2rem;
 font-weight:400;
 margin-top:0.3em;
+@media (max-width: 675px) {
+font-size:1.65rem;
+  }
+  @media (max-width: 500px) {
+font-size:1.2rem;
+  }
 `
 
 const ColorText = styled.span`
@@ -40,24 +52,24 @@ color:${props => props.theme.primaryV};
 
 const Home = () => {
     return (
-            <Hero>
+        <Hero>
             <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                <TextWrapper>
-                    <Header>Hello, I'm <ColorText>Curtis Larsen.</ColorText></Header>
-                    <SubHeader>I'm a <SubColorText>front-end</SubColorText> developer from <SubColorText>Ottawa.</SubColorText></SubHeader>
-                </TextWrapper>
-            </Hero>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <TextWrapper>
+                <Header>Hello, I'm <ColorText>Curtis Larsen.</ColorText></Header>
+                <SubHeader>I'm a <SubColorText>front-end</SubColorText> developer from <SubColorText>Ottawa.</SubColorText></SubHeader>
+            </TextWrapper>
+        </Hero>
     );
 }
 
